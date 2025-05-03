c863646f0a733e83fbe3e9dfe233672f3ea112c0
ananya-0808 <ananyaj0808@gmail.com>
2025-05-03 11:57:53 -0500

    Update test.js

--- test.js
+++ test.js
@@ -9,15 +9,15 @@ const songNameDisplay = document.getElementById('song-name');
 const albumArt = document.getElementById('album-art');
 
 const songs = [
-    { src: 'songs/music-1.mp4', name: 'tranquility', art:'images/music-1.gif' },
-    { src: 'songs/music-2.mp4', name: 'breathe' , art:'images/music-2.gif'},
-    { src: 'songs/music-3.mp4', name: 'loving you', art:'images/music-3.gif' },
-    { src: 'songs/music-4.mp4', name: 'In the Monastery' , art:'images/music-4.gif'},
-    { src: 'songs/music-5.mp4', name: 'Claire De Lune' , art:'images/music-5.gif'},
-    { src: 'songs/music-6.mp4', name: 'Gymnopédie No.1', art:'images/music-6.gif' },
-    { src: 'songs/music-7.mp4', name: 'Imagine' , art:'images/music-7.gif'},
-    { src: 'songs/music-8.mp4', name: 'Bread' , art:'images/music-8.gif'},
-    { src: 'songs/music-9.mp4', name: 'Bliss' , art:'images/music-9.gif'}
+    { src: 'songs/music-1.mp4', name: 'tranquility', art:'music-1.gif' },
+    { src: 'songs/music-2.mp4', name: 'breathe' , art:'music-2.gif'},
+    { src: 'songs/music-3.mp4', name: 'loving you', art:'music-3.gif' },
+    { src: 'songs/music-4.mp4', name: 'In the Monastery' , art:'music-4.gif'},
+    { src: 'songs/music-5.mp4', name: 'Claire De Lune' , art:'music-5.gif'},
+    { src: 'songs/music-6.mp4', name: 'Gymnopédie No.1', art:'music-6.gif' },
+    { src: 'songs/music-7.mp4', name: 'Imagine' , art:'music-7.gif'},
+    { src: 'songs/music-8.mp4', name: 'Bread' , art:'music-8.gif'},
+    { src: 'songs/music-9.mp4', name: 'Bliss' , art:'music-9.gif'}
     
   ];
 
